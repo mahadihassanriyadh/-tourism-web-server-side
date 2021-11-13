9878b3fd0f41758e86f7c3290b7cf94d067da647
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2021-11-13 12:00:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rnw2g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri)
+const app = express();
+const port = process.env.PORT || 5000;
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('tourism');
+        const vacationPackageCollection = database.collection('vacationPackages');
+
+        // GET/FIND API or GET Products API
+        app.get('/vacationPackages', async (req, res) => {
+            const cursor = vacationPackageCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running my Tourism Server')
+})
+
+app.listen(port, ()=>{
+    console.log('Running server on port', port)
+})
