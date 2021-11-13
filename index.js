8c13bc5eacfb7e9e3a36250af10627021a0cb5f8
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2021-11-13 12:24:28 +0600

    cors policy updated

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 
+const app = express();
+const port = process.env.PORT || 5000;
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -10,9 +13,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rnw2g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-const app = express();
-const port = process.env.PORT || 5000;
 
 async function run() {
     try {
